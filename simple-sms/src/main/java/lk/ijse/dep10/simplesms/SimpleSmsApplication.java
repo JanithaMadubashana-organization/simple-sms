9cdc9b8479e4465a0261ce0aa5b8c25411c7081f
janithamadubashana <madubashanajanitha@gmail.com>
2023-06-29 18:57:40 +0530

    add springboot

---
+++ simple-sms/src/main/java/lk/ijse/dep10/simplesms/SimpleSmsApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.simplesms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSmsApplication.class, args);
+	}
+
+}
